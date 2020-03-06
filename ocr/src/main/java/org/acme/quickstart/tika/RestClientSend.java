bd57a3c3a5897a68b90ddc522b55b4de38b864bd
Elvis Rocha <elvishrocha@gmail.com>
2020-03-06 13:44:02 -0300

    Update RestClientSend.java

--- ocr/src/main/java/org/acme/quickstart/tika/RestClientSend.java
+++ ocr/src/main/java/org/acme/quickstart/tika/RestClientSend.java
@@ -3,6 +3,7 @@ package org.acme.quickstart.tika;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import javax.ws.rs.Produces;
 
 
 /**
